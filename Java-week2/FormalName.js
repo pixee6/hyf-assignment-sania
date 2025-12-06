5d1b4d15d6d1827bf9c96a0a7246873520a0cffc
Sania <sania.aslam2514@gmail.com>
2025-12-06 22:00:37 +0100

    Enhance getFullName with gender and validation

--- Java-week2/FormalName.js
+++ Java-week2/FormalName.js
@@ -1,13 +1,28 @@
-function getFullName(firstName, lastName, useFormalName,){ 
-  if (useFormalName === true) {
-      return "Lord " + firstName + " " + lastName;
-    } else {
+function getFullName(firstName, lastName, useFormalName = false, gender) {
+  // Validation
+  if (!firstName || !lastName) {
+    return "Please provide both first and last name";
+  }
+
+  // If no formal name needed
+  if (!useFormalName) {
     return firstName + " " + lastName;
+  }
+
+  // Formal name with gender
+  if (gender === "male") {
+    return "Lord " + firstName + " " + lastName;
+  } else if (gender === "female") {
+    return "Lady " + firstName + " " + lastName;
+  } else {
+    return "Mx. " + firstName + " " + lastName; // gender not provided
+  }
 }
-const fullName1 = getFullName("Benjamin", "Hughes", true, male);
-const fullName2 = getFullName("Aisha", "Khan", false, female);
+
+// TESTS
+const fullName1 = getFullName("Benjamin", "Hughes", true, "male");
+const fullName2 = getFullName("Aisha", "Khan", true, "female");
+const fullName3 = getFullName("Sara", "Ali"); // no formal name
 console.log(fullName1);
 console.log(fullName2);
-
-/*But what if the person is a woman? Describe how you would fix the getFullName so it also works for women
-Answer: I think we should add a gender parameter and than we can add if statement if its female than it will add lady and male to lord.*/
+console.log(fullName3);
